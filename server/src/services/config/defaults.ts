3aa346dd11f85cc3346bc68b9a86cd5f27bf4ee7
seydx <hi@seydx.dev>
2026-07-12 22:35:17 +0200

    fix: update DEFAULT_CONFIG_HOST to use '0.0.0.0'

--- server/src/services/config/defaults.ts
+++ server/src/services/config/defaults.ts
@@ -9,7 +9,7 @@ const listenWithOffset = (host: string, base: number): string => `${host}:${with
 
 // server
 export const DEFAULT_CONFIG_PORT = withOffset(3443);
-export const DEFAULT_CONFIG_HOST = '::';
+export const DEFAULT_CONFIG_HOST = '0.0.0.0';
 export const DEFAULT_CONFIG_SSL: IConfigSSL = {
   certFile: '',
   keyFile: '',
